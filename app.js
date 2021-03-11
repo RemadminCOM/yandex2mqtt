cc83923e860449f55546d1719b335f4ac90edbb8
RemadminCOM <80437907+RemadminCOM@users.noreply.github.com>
2021-03-11 19:01:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -230,6 +230,7 @@ if (statPairs) {
                 case 'power':
                 case 'co2_level':
                 case 'humidity':
+				case 'pressure':
                 case 'temperature':
                     try {
                         devindx = findDevIndexByInstance(device.data.properties, topicType)
